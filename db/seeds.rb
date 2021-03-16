e70ba820afd4101a6c96026cc8dde4fcce232818
Nicholas Turon <nicholasturon@Nicholass-MacBook-Pro.local>
2021-03-16 15:22:18 -0400

    fixed typos for foregin_key in models and typos in seed.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,12 @@
-drivers = Driver.create([
+Driver.create([
   {
     name: 'Driver Dan'
   },
 ])
 
-driver_records = DriverRecord.create([
+DrivingRecord.create([
   {
+    driver_id: 1,
     start_time: DateTime.new(2021, 9, 1, 19, 30),
     end_time: DateTime.new(2021, 9, 1, 19, 30),
     start_location: "not sure how to code this",
