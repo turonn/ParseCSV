479a0446298c115e9e841ac02461e95c5bf7cf8a
Nicholas Turon <nicholasturon@Nicholass-MacBook-Pro.local>
2021-03-16 15:12:30 -0400

    added foregin key relation to link drivers and driving records. Added Annotate gem

--- app/models/Driver.rb
+++ app/models/Driver.rb
@@ -1,3 +1,12 @@
+# == Schema Information
+#
+# Table name: drivers
+#
+#  id         :bigint           not null, primary key
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Driver < ApplicationRecord
-  has_many :driving_records
+  has_many :driving_records, class_name: 'DriveingRecord', foregin_key: :driver_id
 end
--- app/models/DrivingRecord.rb
+++ app/models/DrivingRecord.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: driving_records
+#
+#  id             :bigint           not null, primary key
+#  driver_id      :bigint
+#  start_time     :datetime
+#  end_time       :datetime
+#  start_location :string
+#  end_location   :string
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#
 class DrivingRecord < ApplicationRecord
-  belongs_to :driver
+  belongs_to :driver, class_name: 'Driver', foreign_key: :driver_id
 end
