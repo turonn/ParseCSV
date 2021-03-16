4b35ac6618e5d3ccff3a5141310af253003cf223
Nicholas Turon <nicholasturon@Nicholass-MacBook-Pro.local>
2021-03-16 15:16:49 -0400

    added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+drivers = Driver.create([
+  {
+    name: 'Driver Dan'
+  },
+])
+
+driver_records = DriverRecord.create([
+  {
+    start_time: DateTime.new(2021, 9, 1, 19, 30),
+    end_time: DateTime.new(2021, 9, 1, 19, 30),
+    start_location: "not sure how to code this",
+    end_location: 'not sure how to code this either'
+  }
+])
