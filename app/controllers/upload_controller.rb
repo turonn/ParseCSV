bb481dbead77713e7796cc790fa31f72bd293295
Nicholas Turon <nicholasturon@Nicholass-MacBook-Pro.local>
2021-03-20 17:30:44 -0400

    added import to upload controller and view

--- app/controllers/upload_controller.rb
+++ app/controllers/upload_controller.rb
@@ -1,4 +1,9 @@
 class UploadController < ApplicationController
+
   def index
+  end
+
+  def import
+    redirect_to upload_index_path, notice: "We got 'em, boss!"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: 'driving_records#index'
 
+  post 'upload/import', to: 'upload#import', as: 'import_driving_records'
   resources :upload, only: [:index]
 end
